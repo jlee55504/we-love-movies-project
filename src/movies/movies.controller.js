ee7c9bf35808d7aae0cb356f1fdeb33d1c9074ce
Jason <jlee55504@gmail.com>
2024-09-26 00:45:20 -0700

    Added 'starter code' for the 'movies.controller.js' 'file'.

---
+++ src/movies/movies.controller.js
@@ -0,0 +1,15 @@
+const moviesService = require("./movies.service");
+//const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+// Route handlers
+const list = (req, res) => {
+
+}
+
+const read = (req, res) => {
+
+}
+
+module.exports = {
+
+}
