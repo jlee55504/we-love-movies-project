9ffecee5c2b7299732dc8f207d4fa61c5d0f57be
Jason <jlee55504@gmail.com>
2024-09-26 00:46:29 -0700

    Added 'starter code' for the 'reviews.controller.js' 'file'.

---
+++ src/reviews/reviews.controller.js
@@ -0,0 +1,15 @@
+const reviewsService = require("./reviews.service");
+//const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+// Route handlers
+const update = (req, res) => {
+
+}
+
+const destroy = (req, res) => {
+    
+}
+
+module.exports = {
+
+}
