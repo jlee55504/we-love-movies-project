f39e26cba7c60e8e1ad341b80f1f583c12efeec9
Jason <jlee55504@gmail.com>
2024-09-26 22:05:24 -0700

    Added code to make the 'read' 'route handler' to work.

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,15 +1,37 @@
 const moviesService = require("./movies.service");
-//const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const hasProperties = require("../errors/hasProperties");
+
+// Middleware functions
+const movieExists = async (req, res, next) => {
+    const { movieId } = req.params;
+    if (req.originalUrl.includes("theaters")) res.locals.routePath = "theaters";
+    else if (req.originalUrl.includes("reviews")) res.locals.routePath = "reviews";
+    else if ( req.originalUrl.includes("theaters") || 
+    !req.originalUrl.includes("reviews")) res.locals.routePath = null;
+    const  movie = await moviesService.read(Number(movieId), 
+    res.locals.routePath);
+    if (movie) {
+        res.locals.movie = movie;
+        return next();
+    };
+    next({
+        status: 404,
+        message: "Movie cannot be found",
+    });
+};
+
+
 // Route handlers
 const list = (req, res) => {
 
 }
 
 const read = (req, res) => {
-
+    const { movie: data } = res.locals;
+    res.json({ data });
 }
 
 module.exports = {
-
+    read: [asyncErrorBoundary(movieExists), read],
 }
