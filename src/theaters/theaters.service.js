10db775dc13b4e928cc3f2a0b8ef7a4b4f13641c
Jason <jlee55504@gmail.com>
2024-09-28 16:51:56 -0700

    Added code to make the 'list' 'route handler' work.

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,9 +1,20 @@
 const knex = require("../db/connection");
 
-const list = () => {
+const list = data => {
+  if (data === "movies") {
+    return knex("movies as m")
+      .join("movies_theaters as mt", "mt.movie_id", "m.movie_id")
+      .join("theaters as t", "t.theater_id", "mt.theater_id")
+      .select(knex.raw("t.theater_id, m.*, mt.is_showing"));
+  } else if (data === "theaters") {
+      return knex("movies as m")
+        .join("movies_theaters as mt", "mt.movie_id", "m.movie_id")
+        .join("theaters as t", "t.theater_id", "mt.theater_id")
+        .select(knex.raw("distinct t.theater_id, t.*"));
+  };
+};
 
-}
 
 module.exports = {
     list,
-}
+};
