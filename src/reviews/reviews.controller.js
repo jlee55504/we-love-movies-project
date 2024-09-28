a0ce22086f062aadb632cb2659906ed07d12af19
Jason <jlee55504@gmail.com>
2024-09-27 21:07:49 -0700

    Added code to make the 'update' 'route handler' work, including the proper 'middleware functions'.

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,15 +1,61 @@
 const reviewsService = require("./reviews.service");
-//const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+const VALID_PROPERTIES = [
+    "review_id",
+    "content",
+    "score",
+    "created_at",
+    "updated_at",
+    "critic_id",
+    "movie_id",
+    "critic"
+]
 
-// Route handlers
-const update = (req, res) => {
 
+
+// Middleware functions
+const reviewExists = async (req, res, next) => {
+   const { reviewId } = req.params; 
+  const review = await reviewsService.read(reviewId);
+    if (review) {
+        res.locals.review = review;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `/cannot be found /reviews/${req.params.reviewId}`,
+    })
 }
 
+const hasOnlyValidProperties = async (req, res, next) => {
+    const { data = {} } = req.body;
+    const invalidFields = Object.keys(data).filter(field => !VALID_PROPERTIES.includes(field));
+    if (invalidFields.length) {
+        return next({
+            status: 400,
+            message: `Invalid field(s) ${invalidFields.join(", ")}`,
+        });
+    };
+    next();
+};
+
+// Route handlers
+const update = async (req, res) => {
+    const updatedReview = { 
+      ...res.locals.review,
+          ...req.body.data,
+          review_id: res.locals.review.review_id,
+      };
+      const data = await reviewsService.update(updatedReview);
+    const answer = {...updatedReview, ...data};
+      res.json({  data: answer });
+  }
+  
 const destroy = (req, res) => {
     
 }
 
 module.exports = {
-
-}
+    update: [asyncErrorBoundary(reviewExists), hasOnlyValidProperties,  asyncErrorBoundary(update)]
+};
