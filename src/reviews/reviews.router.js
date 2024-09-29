083f4e0f4b1ad80fe4f6838ebef143df83c822d3
Jason <jlee55504@gmail.com>
2024-09-28 18:23:21 -0700

    Added and used the 'CORS' 'method'.

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,10 +1,12 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
 
 router.route("/:reviewId")
+    .all(cors())    
     .put(controller.update)
-    .delete(controller.delete)
+    .delete( controller.delete)
     .all(methodNotAllowed);
     
 module.exports = router;
