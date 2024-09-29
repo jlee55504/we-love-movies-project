7337d316c135804f1b299265bb000caea54728ba
Jason <jlee55504@gmail.com>
2024-09-28 18:30:29 -0700

    Added and used the 'CORS' 'method'.

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,9 +1,10 @@
 const router = require("express").Router({ mergeParams: true });
 const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
 
 router.route("/")
-  .get(controller.list)
+  .get(cors(), controller.list)
   .all(methodNotAllowed);
 
 module.exports = router;
